cce38be03d46f48a7a8f9840922cd321a6299489
Hutheifa <ihudheifa87@gmail.com>
2025-05-26 13:27:35 +0300

    files

---
+++ script.js
@@ -0,0 +1,14 @@
+// console.log("Hello, World!");
+// alert("This is a simple alert message!");
+// prompt("Please enter your name:", "Guest");
+
+/** 
+ *multiline comment
+
+ used to add some documentation or explanation
+ * 
+*/
+
+function sampleFunction() {}
+
+console.log("This is a sample function.");
---
+++ variables.js
@@ -0,0 +1,35 @@
+// 1.Declaration
+
+//Declaring variables in JavaScript can be done using three keywords: var, let, and const. Each has its own scope and use cases.
+// -var
+// -let
+// -const
+
+// a.var
+var variableName;
+
+console.log(variableName);
+
+variableName = "hey there!";
+console.log(variableName);
+
+// b.let
+let variableName2;
+
+variableName2 = "Hey,there!";
+console.log(variableName2);
+
+// console.log(variableName2);
+
+// c.const
+const variableName3 = "Hello,world!";
+console.log(variableName3);
+
+// console.log(variableName3);
+
+// 2.ASSIGNMENT
+
+// 3.INITIALIZATION
+
+// VARIABLE RULES
+// DATA TYPES
