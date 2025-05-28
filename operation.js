11749072008608135b8bc84a0c4f210a103f18c3
Hutheifa <ihudheifa87@gmail.com>
2025-05-28 18:06:24 +0300

    files

---
+++ operation.js
@@ -0,0 +1,57 @@
+// let age = 18;
+
+// if (age >= 18) {
+//   console.log("You can vote.");
+// } else {
+//   console.log("You're too young.");
+// }
+
+//challenge
+
+// let username = "hutheifa";
+// let password = "jsRocks";
+
+// if (username === "hutheifa" && password === "jsRocks") {
+//   console.log("Login successful");
+// } else {
+//   console.log("Try again");
+// }
+
+// let username = "wronguser";
+// let password = "jsRocks";
+
+// if (username === "wronguser" && password === "jsRock") {
+//   console.log("Login successful");
+// } else {
+//   console.log("Try again");
+// }
+
+// let username = "hutheifa";
+// let password = "123";
+
+// if (username === "hutheifa" && password === "123") {
+//   console.log("Login successful");
+// } else {
+//   console.log("Try again");
+// }
+
+let username = "admin";
+let password = "secret";
+
+if (username === "admin" && password === "secret") {
+  console.log("welcome back");
+} else {
+  console.log("invalid username or password");
+}
+
+//false value examples
+
+// o undefined emptystrings null
+
+const str = "hello";
+
+const str2 = " world";
+
+const str3 = str + str2;
+
+console.log(str3);
