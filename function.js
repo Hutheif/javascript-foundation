769ddf2f8b48974671094b7ca8e0195c035ff662
Hutheifa <ihudheifa87@gmail.com>
2025-06-02 22:20:15 +0300

    files

---
+++ function.js
@@ -0,0 +1,8 @@
+// function is a set of statement that performs a task or calculates a value.
+
+function greet(name, lastname) {
+  console.log("Hello ", name);
+}
+
+greet("john"); // Calling the function to execute it
+greet("Alice"); // Calling the function with an argument
--- operation.js
+++ operation.js
@@ -35,23 +35,77 @@
 //   console.log("Try again");
 // }
 
-let username = "admin";
-let password = "secret";
+// let username = "admin";
+// let password = "secret";
 
-if (username === "admin" && password === "secret") {
-  console.log("welcome back");
-} else {
-  console.log("invalid username or password");
-}
+// if (username === "admin" && password === "secret") {
+//   console.log("welcome back");
+// } else {
+//   console.log("invalid username or password");
+// }
 
 //false value examples
 
 // o undefined emptystrings null
 
-const str = "hello";
+// const str = "hello";
 
-const str2 = " world";
+// const str2 = " world";
 
-const str3 = str + str2;
+// const str3 = str + str2;
 
-console.log(str3);
+// console.log(str3);
+
+// const s = "hello World !";
+
+// console.log(s.split(""));
+
+// const s = "hello, technology, computer, science";
+// console.log(s.split(", "));
+
+//Arrays-variables that store multiple values
+
+// Arrays are always zero based in every language
+// const fruits = ["apple", "banana", "orange", "grape"];
+
+// fruits[5] = "avocado"; // Adding a new element at index 5
+
+// fruits.push("mango"); // Adding a new element at the end of the array
+// fruits.unshift("strawberry"); // Adding a new element at the beginning of the array
+// fruits.pop();
+
+// // console.log(Array.isArray(fruits));check if something is an array
+// console.log(fruits.indexOf("banana")); // Output: 1
+
+// console.log(fruits); // Output: ["apple", "banana", "orange", "grape", undefined, "avocado"]
+
+// object literals
+// const person = {
+//   firstname: "John",
+//   lastname: "Doe",
+//   age: 30,
+//   hobbies: ["reading", "gaming", "coding"],
+//   address: {
+//     street: "123 Main St",
+//     city: "Anytown",
+//     country: "USA",
+//   },
+// };
+
+// console.log(person.firstname, person.lastname); // Output: John
+// console.log(person.hobbies[2]); // Output: gaming
+
+// console.log(person.address.city); // Output: Anytown
+
+// destructuring creates a new variable with the same name as the property
+// const {
+//   firstname,
+//   lastname,
+//   address: { country },
+// } = person;
+// console.log(country); // Output: John Doe
+
+// // add property
+
+// person.email = "john@gmail.com";
+// console.log(person.email); // Output:
